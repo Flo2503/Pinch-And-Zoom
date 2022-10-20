214c4e658e3f73208951593929f1b5c53d8c17d9
Flo2503 <bruel.florent@gmail.com>
2022-10-20 17:18:41 +0200

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Florent on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
