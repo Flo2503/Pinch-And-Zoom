c72854f4a843bc86b902c8fad8a09aabf1b0b240
Flo2503 <bruel.florent@gmail.com>
2022-10-20 18:09:48 +0200

    Added image in navigation view

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            ZStack {
+                // MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                
+            } //: ZSTACK
+            .navigationTitle("Pinch and Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+        } //: NAVIGATION
+        .navigationViewStyle(.stack)
     }
 }
 
