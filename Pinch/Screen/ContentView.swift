964848e935a93236b9a9fe51de7997c4085a76ba
Flo2503 <bruel.florent@gmail.com>
2022-10-20 18:32:06 +0200

    Added animation on appear and created double tap gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTY
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    
     var body: some View {
         NavigationView {
             ZStack {
@@ -18,8 +22,27 @@ struct ContentView: View {
                     .cornerRadius(10)
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .scaleEffect(imageScale)
+                // MARK: - 1. TAP GESTURE
+                    .onTapGesture(count: 2, perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    })
                 
             } //: ZSTACK
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1.4)) {
+                    isAnimating = true
+                }
+            })
             .navigationTitle("Pinch and Zoom")
             .navigationBarTitleDisplayMode(.inline)
         } //: NAVIGATION
