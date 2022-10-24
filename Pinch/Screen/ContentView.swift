d66739d8185bcfba0a86d21136f40f8dc0b5fe77
Flo2503 <bruel.florent@gmail.com>
2022-10-24 18:48:04 +0200

    Finished info panel view

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -24,6 +24,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ZStack {
+                Color.clear
                 // MARK: - PAGE IMAGE
                 Image("magazine-front-cover")
                     .resizable()
@@ -66,6 +67,13 @@ struct ContentView: View {
             })
             .navigationTitle("Pinch and Zoom")
             .navigationBarTitleDisplayMode(.inline)
+            // MARK: - INFO PANEL
+            .overlay(
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                .padding(.horizontal)
+                .padding(.top, 30),
+                alignment: .top
+            )
         } //: NAVIGATION
         .navigationViewStyle(.stack)
     }
